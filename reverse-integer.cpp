73342405a7364fa317c716e527fb7af2a9c41adc
Bryan Sauter <bryan.sauter.tsmm@gmail.com>
2020-11-26 01:54:21 -0500

    Updated solution.

--- reverse-integer.cpp
+++ reverse-integer.cpp
@@ -1,22 +1,21 @@
 class Solution {
+    //https://leetcode.com/problems/reverse-integer
 public:
-    int reverse(int x) {
-        int result = 0, overFlow = 0;
-        //keep going till there is nothing left of the input
-        while(x != 0)
-        {
-            //add it to the tens place
-            cout << overFlow << endl;
-            cout << (overFlow > INT_MAX / 10) << endl;
-            cout << (overFlow < INT_MIN / 10) << endl;
-            if( overFlow > INT_MAX / 10 || overFlow < INT_MIN / 10 )
-                return 0;
-            overFlow = result * 10 + (x % 10);
-            //we didn't overflow celebrate!
-            result = overFlow;
-            //cut off the tens place
-            x /= 10;
-        }
-        return result;
-    }
+    int reverse(int x) {
+        int result = 0, overFlow = 0;
+        //keep going till there is nothing left of the input
+        while(x != 0)
+        {
+            //check for potential over or under flow...
+            if( overFlow > INT_MAX / 10 || overFlow < INT_MIN / 10 )
+                return 0;
+            //add it to the tens place with remainder
+            overFlow = result * 10 + (x % 10);
+            //we didn't overflow celebrate!
+            result = overFlow;
+            //cut off the tens place
+            x /= 10;
+        }
+        return result;
+    }
 };
