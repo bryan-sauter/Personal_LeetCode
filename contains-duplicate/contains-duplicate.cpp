678583f99badc43bdac47912e09fac0206e71526
Bryan Sauter <bryan.sauter.tsmm@gmail.com>
2020-12-05 02:51:06 -0500

    working commit - Created using LeetHub

---
+++ contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int> countStore;
+        for(auto i : nums)
+        {
+            countStore[i]++;
+            if(countStore[i] == 2)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
