91cfb3b5d517384aaee28d3abafd454abaa9bbc8
Bryan Sauter <bryan.sauter.tsmm@gmail.com>
2020-12-11 22:43:44 -0500

    Time: 8 ms (95.67%), Space: 9.4 MB (78.34%) - LeetHub

---
+++ two-sum/two-sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> result;
+        unordered_map<int, int> needsMap;
+​
+        for(int i = 0; i < nums.size(); ++i)
+        {
+            if(auto idx = needsMap.find(nums[i]); idx != needsMap.end())
+            {
+                result.push_back(idx->second);
+                result.push_back(i);
+                return result;
+            }
+            needsMap.emplace(make_pair(target - nums[i], i));
+        }
+        return result;
+    }
+};
