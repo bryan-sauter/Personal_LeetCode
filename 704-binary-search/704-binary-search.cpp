eb89c72f7c355c3b9fd41b76e8d40ed0b15928ca
Bryan Sauter <bryan.sauter.tsmm@gmail.com>
2022-06-08 21:59:37 -0400

    Time: 72 ms (13.72%), Space: 27.6 MB (13.32%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -1,11 +1,22 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        for(unsigned int i = 0u; i < nums.size(); ++i)
+    int search(vector<int>& nums, int target) {//fun fact sequential still passes
+        int low = 0, high = nums.size() - 1, mid = 0;
+        
+        while (low <= high)
         {
-            if( nums[i] == target )
+            mid = low + (high - low) / 2;//prevent overflow
+            if(target == nums[mid])
             {
-                return i;
+                return mid;
+            }
+            else if(target > nums[mid])
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
             }
         }
         return -1;
