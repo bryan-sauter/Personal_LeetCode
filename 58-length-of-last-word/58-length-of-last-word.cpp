3f9b289fe41f423b89b303f2b07848fe5bf50917
Bryan Sauter <bryan.sauter.tsmm@gmail.com>
2022-06-10 14:01:01 -0400

    Time: 3 ms (53.27%), Space: 6.6 MB (11.44%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.cpp
+++ 58-length-of-last-word/58-length-of-last-word.cpp
@@ -4,13 +4,13 @@ public:
         int count = 0;
         for(int i = s.length() - 1; i >= 0; --i)
         {
-            if( s[i] == ' ' && count)
-            {
-                break;
-            }
-            else if( s[i] != ' ')
+            if( s[i] != ' ' )
             {
                 count++;
+            }
+            else if( s[i] == ' ' && count)
+            {
+                break;
             }
         }
         return count;
