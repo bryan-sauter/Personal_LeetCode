08fade4c48b4702de47d08ce30295e51398e01ab
Bryan Sauter <bryan.sauter.tsmm@gmail.com>
2022-06-09 22:37:53 -0400

    Time: 40 ms (86.86%), Space: 28.1 MB (92.52%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.cpp
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int numPairsDivisibleBy60(vector<int>& time) {
         int retCount = 0, pair = 0;
-        vector<int> timePair(60,0);//track missing pairs, via how many hits?
+        vector<int> timePair(60,0);//track missing pairs, via how many hits
         for(const int timeVal : time)
         {
             pair = timeVal % 60;//how far are we from being divisible by 60?
